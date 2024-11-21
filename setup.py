bdd85ff9e321f32b1b5d5d08e580918a493ddf89
grez72 <grez72@gmail.com>
2024-11-21 16:37:35 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+   
+setup(
+    name = 'model_rearing_rsa',
+    packages = find_packages(),
+    install_requires = [
+        # 'lightning-sdk',
+        # 'litdata @ git+https://github.com/Lightning-AI/litdata.git',
+    ]
+)
